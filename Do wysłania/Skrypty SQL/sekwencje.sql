0208a4c4d9234793c0d408ad4b0d0e8978e27648
JJ807 <jakubstrugaa@gmail.com>
2021-02-13 01:57:55 +0100

    deleting spare dir

--- Do wysłania/Skrypty SQL/sekwencje.sql
+++
@@ -1,9 +0,0 @@
-create sequence "Rola_osoby_id_rola_osoby_seq";
-
-alter sequence "Rola_osoby_id_rola_osoby_seq" owner to npbkqkcz;
-
-create sequence "Ocena_id_ocena_seq";
-
-alter sequence "Ocena_id_ocena_seq" owner to npbkqkcz;
-
-
